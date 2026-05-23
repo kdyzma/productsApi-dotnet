cad01f834b3b0163e919aef1f2dc12faef98ae4f
Krystyna Dyzma <dyzma.krystyna@gmail.com>
2026-05-23 12:07:23 +0200

    Add CORS

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@ using ProductsApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ProductContext>(options =>
     options.UseInMemoryDatabase("Products"));
@@ -11,13 +21,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
